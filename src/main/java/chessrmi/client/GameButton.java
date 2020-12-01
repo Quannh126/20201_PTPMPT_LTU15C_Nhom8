bc8846d3195aa554fbfd5ce231da6765278ee488
huyvu1104 <45757357+huyvu1104@users.noreply.github.com>
2020-12-01 12:26:51 +0700

    client

---
+++ src/main/java/chessrmi/client/GameButton.java
@@ -0,0 +1,14 @@
+package chessrmi.client;
+
+import constants.GameColor;
+
+import javax.swing.*;
+
+public class GameButton extends JButton {
+    public GameButton(String title){
+        super(title);
+        setBackground(GameColor.BUTTON_BACKGROUND);
+        setForeground(GameColor.BUTTON_FOREGROUND);
+    }
+}
+
