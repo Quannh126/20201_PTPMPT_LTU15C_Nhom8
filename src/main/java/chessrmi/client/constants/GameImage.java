0984d7bf397817100caae226050059dcfbe134c7
Quan <quan96126@bkcs.hust.edu.vn>
2020-12-12 19:44:10 +0700

    fix cay thu muc

--- src/main/java/chessrmi/client/constants/GameImage.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-import javax.swing.*;
-
-public interface GameImage {
-    ImageIcon ICON_HUST = new ImageIcon(GameImage.class.getResource("/res/hust.png"));
-}
--- src/main/java/chessrmi/client/constants/Session.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public interface Session {
-    String SESSION_ONE="Session1";
-    String SESSION_TWO="Session2";
-    String SESSION_THREE="Session3";
-}
