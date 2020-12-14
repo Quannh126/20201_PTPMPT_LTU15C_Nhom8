93ae5d5e611c64be24cddfd4333bdae5579e4156
Quan <quan.nh148983@sis.hust.edu.vn>
2020-12-14 23:31:31 +0700

    up to 3 session

--- Source/Server/ChessServer.java
+++ Source/Server/ChessServer.java
@@ -4,12 +4,14 @@ import java.rmi.registry.*;
 
 public class ChessServer {
 
+    private static final long serialVersionUID = 1L;
+
     public static void main(String[] args) {
-        // server = new ChessServer();
+
         if (hostGames()) {
-            System.out.println("\n...Hosting successful!");
+
         } else {
-            System.out.println("\n...Hosting Failed!");
+
         }
     }
 
@@ -17,21 +19,31 @@ public class ChessServer {
         boolean success = false;
         try {
             // create GameSession objects to host
-            GameSession sesh = new GameSession();
+            GameSession sesh1 = new GameSession();
+            GameSession sesh2 = new GameSession();
+            GameSession sesh3 = new GameSession();
 
             // Create 3 registry objects for each session
-            Registry registry;
-
-            registry = LocateRegistry.getRegistry();
+            Registry registry1;
+            Registry registry2;
+            Registry registry3;
+            // registry = LocateRegistry.getRegistry();
 
             // Start up the registries
-            registry = LocateRegistry.createRegistry(4200);
+            registry1 = LocateRegistry.createRegistry(4300);
             System.out.println("registry1 created at port 4200");
+            registry2 = LocateRegistry.createRegistry(4301);
+            System.out.println("registry2 created at port 4201");
+            registry3 = LocateRegistry.createRegistry(4302);
+            System.out.println("registry2 created at port 4202");
 
             // Bind each Session with a registry and give it a name
-            registry.rebind("Session1", sesh);
-            System.out.println("Sesh up and running");
-
+            registry1.rebind("Session1", sesh1);
+            // server.lab2.setText("Sesh1 up and running");
+            registry2.rebind("Session2", sesh2);
+            // server.lab3.setText("Sesh2 up and running");
+            registry3.rebind("Session3", sesh3);
+            // server.lab4.setText("Sesh3 up and running");
             success = true;
         } catch (Exception e) {
             e.printStackTrace();
